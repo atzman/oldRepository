d968c890da34818f7491ac552396f7a4d431d588
luke <Lukas H>
2019-08-29 14:16:33 +0200

    master

--- _site/assets/js/scrollOffset.js
+++ _site/assets/js/scrollOffset.js
@@ -1,26 +1,34 @@
 $(window).on('load', function() {
-  
+
+  $('.after-scroll-top-banner-container').css( "display", "flex" );
   $('.atzman-intro-container').css( "opacity", "1" );
   $('.navigation-bar').css( "opacity", "1" );
 
-  var scroll_position_banner = 300 - ( $(window).scrollTop()* (5/6) ) ;
+  setTimeout(function(){},500);
+
+  $('body').css( "overflow", "auto");
+  $('body').css( "overflow-x", "hidden");
+  $('html').css( "overflow", "auto");
+  $('html').css( "overflow-x", "hidden");
+
+  var scroll_position_banner = 200 - ( $(window).scrollTop()* (5/6) ) ;
 
   $('.atzman-intro-container').css( "margin-top", scroll_position_banner );
 
-  if( (800 - $(window).scrollTop()) > 0 ) {
-    var scroll_position = 800 - ( $(window).scrollTop() ) ;
+  if( (1000 - $(window).scrollTop()) > 0 ) {
+    var scroll_position = 1000 - ( $(window).scrollTop() ) ;
   } else { var scroll_position = 0; }
 
   $('.navigation-bar').css( "margin-top", scroll_position );
 
   $(window).scroll(function () {
 
-    var scroll_position_banner = 300 - ( $(window).scrollTop() * (5/6) ) ;
+    var scroll_position_banner = 200 - ( $(window).scrollTop() * (5/6) ) ;
 
     $('.atzman-intro-container').css( "margin-top", scroll_position_banner );
 
-    if( (800 - $(window).scrollTop()) > 0 ) {
-      var scroll_position = 800 - ( $(window).scrollTop() ) ;
+    if( (1000 - $(window).scrollTop()) > 0 ) {
+      var scroll_position = 1000 - ( $(window).scrollTop() ) ;
     } else {
       var scroll_position = 0; }
 
